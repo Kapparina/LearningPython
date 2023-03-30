7545c0327e3a7861ab750c70c5176e6a233158ee
Kapparina <116474667+Kapparina@users.noreply.github.com>
2023-03-31 00:05:32 +1100

    Included PyCharm Settings and Fira Code Font

---
+++ Modules/PythonRepo/lists_and_tuples.py
@@ -0,0 +1,11 @@
+# Example of list with For loop:
+
+x = [1, 2, 3, 4]
+y = x[:]
+print(x.pop(), x)
+print(y)
+for num, element in enumerate(x):
+    print(num, element)
+
+# Example with While loop:
+
