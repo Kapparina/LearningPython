55b61553d0fc2438a179787e4193320c99860d3a
Kapparina <116474667+Kapparina@users.noreply.github.com>
2023-04-01 10:47:20 +1100

    Learning WIP

--- Modules/PythonRepo/handling_exceptions.py
+++ Modules/PythonRepo/handling_exceptions.py
@@ -2,6 +2,13 @@
 
 # raise Exception("bad")
 def func_exception():
-    raise FileExistsError("FILENOOOO") # Call me for a demonstration.
+    raise FileExistsError("FILENOOOO")  # Call me for a demonstration.
+
 
 # Now for Try:
+try:
+    x = 7 / 0
+except Exception as e:
+    print(e)
+finally:
+    print("finally")
