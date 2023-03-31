081596e30dbd8e54c9c1dce7f71b4675ae5f80e1
Kapparina <116474667+Kapparina@users.noreply.github.com>
2023-04-01 02:14:06 +1100

    Learning WIP

--- Modules/PythonRepo/main.py
+++ Modules/PythonRepo/main.py
@@ -3,7 +3,17 @@ import os
 from pathlib import Path
 import sys
 import pandas as pd
+import cProfile
+import pytest
+import time
+
+start = time.time()
+
 
 # Define and print "workbook.csv".
 csvfile = pd.read_csv("workbook.csv")
-print(csvfile.head().to_string())
+print(csvfile.head(5).to_string())
+
+end = time.time()
+
+print("Execution time: \n", (end-start) * 10**3 / 1000, "s")
