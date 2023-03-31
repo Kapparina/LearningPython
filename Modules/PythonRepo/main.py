81f626652bf1468afc8277a05ae11bebbe634fdd
Kapparina <116474667+Kapparina@users.noreply.github.com>
2023-03-31 19:13:40 +1100

    Learning WIP

--- Modules/PythonRepo/main.py
+++ Modules/PythonRepo/main.py
@@ -4,10 +4,6 @@ from pathlib import Path
 import sys
 import pandas as pd
 
-
-# Function to print and amend working directory.
-relpath = Path(__file__).parent / "Modules"
-
 # Define and print "workbook.csv".
-csvfile = pd.read_csv(relpath & "/workbook.csv")
+csvfile = pd.read_csv("workbook.csv")
 print(csvfile.head().to_string())
