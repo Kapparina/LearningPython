c180e1fd1a9ec9b6053b78d92be879873c1fe04f
Kapparina <116474667+Kapparina@users.noreply.github.com>
2023-04-01 17:39:14 +1100

    Learning WIP

--- Modules/PythonRepo/main.py
+++ Modules/PythonRepo/main.py
@@ -1,24 +1,24 @@
-# Import os library.
 import time
-import os
-from pathlib import Path
-import sys
 import pandas as pd
-import cProfile
-import pytest
 import pyexcel as p
-
 
 start = time.time()
 
 
-# Define and print "workbook.csv".
-# csvfile = pd.read_csv("workbook.csv")
-# print(csvfile.head(5).to_string())
-sheet = p.get_sheet(file_name="workbook.csv")
-print(sheet)
+def pandas_csv():  # Reading a CSV file with Pandas...
+    csvfile = pd.read_csv("workbook.csv")
+    print(csvfile.head(5).dtypes.to_string())
+    csvfile.to_excel("../workbooktemp.xlsx", sheet_name = "test", index = False)
 
+def pyexcel_csv():  # Reading a CSV file with PyExcel...
+    sheet = p.get_sheet(file_name="../Automobile.csv")
+    sheet.row += ["This", "row", "is", "for", "testing", "purposes"]
+    sheet.column += ["Test Col"]
+    print(sheet)
+
+
+pandas_csv()
 
 end = time.time()
 
-print("Execution time: \n", (end-start) * 10**3 / 1000, "s")
+print("Execution time: \n", (end - start) * 10 ** 3 / 1000, "s")
