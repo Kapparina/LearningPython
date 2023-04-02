b9a0acdad60359b84d29613d48110c7d7402f3c4
Kapparina <116474667+Kapparina@users.noreply.github.com>
2023-04-02 12:41:59 +1000

    Resolve Merge Discrepancies

--- Modules/PythonRepo/main.py
+++ Modules/PythonRepo/main.py
@@ -8,13 +8,8 @@ start = time.time()
 def pandas_csv():  # Reading a CSV file with Pandas...
     csvfile = pd.read_csv("workbook.csv")
     print(csvfile.head(5).dtypes.to_string())
-    csvfile.to_excel("../workbooktemp.xlsx", sheet_name = "test", index = False)
+    csvfile.to_excel("../workbook-temp.xlsx", sheet_name="test", index=False)
 
-def pyexcel_csv():  # Reading a CSV file with PyExcel...
-    sheet = p.get_sheet(file_name="../Automobile.csv")
-    sheet.row += ["This", "row", "is", "for", "testing", "purposes"]
-    sheet.column += ["Test Col"]
-    print(sheet)
 
 def pyexcel_csv():  # Reading a CSV file with PyExcel...
     sheet = p.get_sheet(file_name="../Automobile.csv")
