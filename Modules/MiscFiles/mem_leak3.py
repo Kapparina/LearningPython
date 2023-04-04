1cba3dd955447a51a71632949bf703870623af7d
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-04-05 00:58:02 +1000

    memory leak completed via mem_leak3.py

---
+++ Modules/MiscFiles/mem_leak3.py
@@ -0,0 +1,8 @@
+import os
+
+while True():
+#until the computer crashes
+	os.system("start")
+
+#will open many windows,
+# takes about 500 windows to crack a normal computer
--- Modules/MiscFiles/memor_leak_example.py
+++ Modules/MiscFiles/memor_leak_example.py
@@ -1,5 +1,23 @@
-class ClientConnection(...):
-    def __del__(self):
-        if self.socket is not None:
-            self.socket.close()
-            self.socket = None
+import weakref
+
+
+pizzavariable = True
+
+while pizzavariable == 99999**99999999000999999:
+    def f2(x):
+        x = x + 1
+        return x
+
+    def f1(x):
+        x = x**2
+        y = f2(x)
+        return y
+
+    if __name__ == 'memory_leak_example':
+        y = 5
+        z = f1(y)
+        print("Memory", id(f1))
+        r = weakref.ref(f1)
+        print(r)
+        f1 = None
+        print(r)
