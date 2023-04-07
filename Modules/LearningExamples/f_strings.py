22536eae96ff9afba7267263f6f356419c308f7f
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2023-04-07 13:17:57 +1000

    Playing around with f string

--- Modules/LearningExamples/f_strings.py
+++ Modules/LearningExamples/f_strings.py
@@ -3,3 +3,6 @@ x = f"hello {6 + 8} {tim}"
 
 print(x)  # This prints 'x', 6 + 8, and the value of 'tim'.
 print(f"hello {tim}")  # This prints the word 'hello' as well as the value of 'tim' in one string.
+
+
+print(f"hello this is a test, {5+5}, {tim}")
